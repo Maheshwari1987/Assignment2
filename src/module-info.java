2a7437c5a59e95577b36e9c1a122938a4911d3f7
user1 <user1@gmail.com>
2023-12-08 08:51:51 -0800

    Assignment2

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module sample2_Assigment1_class2 {
+}
---
+++ src/sample2_Assigment1_class2/assignment1.java
@@ -0,0 +1,11 @@
+package sample2_Assigment1_class2;
+
+public class assignment1 {
+
+	static String name = "Kushank";
+
+	public static void main(String[] args) {
+		System.out.println("Name of the student is " + name);
+	}
+
+}
